e21fe8200c19b5769460a1b63ad1c1e9725a4f44
Tim Bornemann <timbornemann@users.noreply.github.com>
2025-03-25 18:40:24 +0100

    Change texts

--- Public/Grillhuette/Reservierung/includes/header.php
+++ Public/Grillhuette/Reservierung/includes/header.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grillhütte Reservierungssystem</title>
+    <title>Reservierungssystem der Grillhütte Waldems Reichenbach</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
@@ -16,13 +16,16 @@ require_once __DIR__ . '/config.php';
     <header class="bg-dark text-white">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="<?php echo getRelativePath('home'); ?>">Grillhütte Reservierung</a>
+                <a class="navbar-brand" href="<?php echo getRelativePath('home'); ?>">Grillhütte Waldems Reichenbach</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Navigation umschalten">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
+                    <li class="nav-item">
+                            <a class="nav-link" href="/Grillhuette">Informationen</a>
+                    </li>
+                    <li class="nav-item">
                             <a class="nav-link" href="<?php echo getRelativePath('home'); ?>">Startseite</a>
                         </li>
                         <?php if (isset($_SESSION['user_id'])): ?>
