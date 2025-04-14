7b57a4858ff6466a74ce3054226234f1492a0509
Tim Bornemann <timbornemann@users.noreply.github.com>
2025-04-14 20:54:57 +0200

    Add text

--- Public/Impressum/index.php
+++ Public/Impressum/index.php
@@ -62,8 +62,13 @@
               <div class="item features-without-image col-12">
                 <div class="item-wrapper">
                   <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Vertreten durch:</strong></h4>
-                  <p class="mbr-text mbr-fonts-style display-7">Herr Sascha Eder, 1. Vorsitzender, </script>Telefon: <script>document.write('+4<!--|-->9-1<!--|-->51-548<!--|--><!--|--><!--|-->4<!--|--><!--|-->63<!--|-->7<!--|-->5');</script>
-                   <br>Herr Stephan Debus, 2. Vorsitzender</p>
+                  <p class="mbr-text mbr-fonts-style display-7">Herr Sascha Eder, 1. Vorsitzender, </script>Telefon:
+                    <script>document.write('+4<!--|-->9-1<!--|-->51-548<!--|--><!--|--><!--|-->4<!--|--><!--|-->63<!--|-->7<!--|-->5');</script>
+                    <br>Herr Stephan Debus, 2. Vorsitzender
+                  </p>
+                  <p class="mbr-text mbr-fonts-style display-7"> <strong>Vertretungsberechtigt gemäß § 26
+                      BGB:</strong><br>
+                    Beide Vorsitzenden sind jeweils einzelvertretungsberechtigt.<br><br></p>
                 </div>
               </div>
               <div class="item features-without-image col-12">
@@ -73,88 +78,94 @@
                   </h4>
                   <p class="mbr-text mbr-fonts-style display-7">E-Mail:
                     <script>document.write('i<!--|--><!--|-->nfo<!--|-->' + '@<!--|-->' + 'fe<!--|-->u<!--|-->erweh<!--|-->r-wald<!--|-->em<!--|-->s-rei<!--|-->che<!--|-->n<!--|-->ba<!--|-->ch.<!--|-->de');</script>
-                </div >
-              </div >
-                        <div class="item features-without-image col-12">
-                          <div class="item-wrapper">
-                            <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5">
-                              <strong>Registereintrag:</strong>
-                            </h4>
-                            <p class="mbr-text mbr-fonts-style display-7">Eintragung im Vereinsregister.<br>Registergericht:
-                              Amtsgericht Wiesbaden<br>Registernummer: VR 5214</p>
-                            </div>
-                          </div>
-                          <div class="item features-without-image col-12">
-                            <div class="item-wrapper">
-                              <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5">
-                                <strong>Steuernummer:</strong>
-                              </h4>
-                              <p class="mbr-text mbr-fonts-style display-7">04 250 51841, Finanzamt Rheingau-Taunus<br></p>
-                            </div>
-                          </div>
-                          <div class="item features-without-image col-12">
-                            <div class="item-wrapper">
-                              <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Social Media:</strong></h4>
-                              <p class="mbr-text mbr-fonts-style display-7">Dieses Impressum gilt auch für unsere Social Media
-                                Auftritte bei:<br><strong>Instagram</strong>: <a
-                                  href="https://www.instagram.com/feuerwehrreichenbach/"
-                                  class="text-secondary">https://www.instagram.com/feuerwehrreichenbach/</a><br><strong>Facebook</strong>:
-                                    <a href="https://www.facebook.com/groups/163127135137/"
-                                      class="text-secondary">https://www.facebook.com/groups/163127135137/</a><br></p>
-                                  </div>
-                                </div>
-                                <div class="item features-without-image col-12">
-                                  <div class="item-wrapper">
-                                    <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Haftung für Inhalte</strong>
-                                    </h4>
-                                    <p class="mbr-text mbr-fonts-style display-7">Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für
-                                      eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG
-                                      sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
-                                      Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
-                                      hinweisen.<br><br>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach
-                                        den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab
-                                        dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
-                                        entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br></p>
-                                      </div>
-                                      </div>
-                                      <div class="item features-without-image col-12">
-                                        <div class="item-wrapper">
-                                          <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Urheberrecht</strong></h4>
-                                          <p class="mbr-text mbr-fonts-style display-7">Die durch die Seitenbetreiber erstellten Inhalte und
-                                            Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung,
-                                            Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
-                                            schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
-                                            sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.<br></p>
-                                        </div>
-                                      </div>
-                                      <div class="item features-without-image col-12">
-                                        <div class="item features-without-image col-12">
-                                          <div class="item-wrapper">
-                                            <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5">
-                                              <strong>Datenschutzerklärung:</strong></h4>
-                                            <p class="mbr-text mbr-fonts-style display-7">Die Datenschutzerklärung ist in vollem Umfang auf unserer Webseite verfügbar und kann unter folgendem Link eingesehen werden: <a
-                                              href="/Datenschutz" class="text-secondary"><?php echo $_SERVER['HTTP_HOST'] ?>/Datenschutz</a>.</p>
-                                          </div>
-                                        </div>
-                                        <div class="item-wrapper">
-                                          <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Online-Streitbeilegung
-                                            (OS):</strong></h4>
-                                          <p class="mbr-text mbr-fonts-style display-7">Die Europäische Kommission hat eine Plattform zur
-                                            Online-Streitbeilegung (OS) eingerichtet, die Sie unter folgendem Link erreichen können: <a href="https://ec.europa.eu/consumers/odr"
-                                              class="text-secondary">https://ec.europa.eu/consumers/odr</a>. Unsere E-Mail-Adresse ist im Impressum oben aufgeführt.</p>
-                                        </div>
-                                      </div>
-                                  </div>
-                                </div>
-                            </div>
-                          </div>
-                        </div>
-  </section >
+                  </p>
+                </div>
+              </div>
+              <div class="item features-without-image col-12">
+                <div class="item-wrapper">
+                  <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5">
+                    <strong>Registereintrag:</strong>
+                  </h4>
+                  <p class="mbr-text mbr-fonts-style display-7">Eintragung im Vereinsregister.<br>Registergericht:
+                    Amtsgericht Wiesbaden<br>Registernummer: VR 5214</p>
+                </div>
+              </div>
+              <div class="item features-without-image col-12">
+                <div class="item-wrapper">
+                  <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5">
+                    <strong>Steuernummer:</strong>
+                  </h4>
+                  <p class="mbr-text mbr-fonts-style display-7">04 250 51841, Finanzamt Rheingau-Taunus<br></p>
+                </div>
+              </div>
+              <div class="item features-without-image col-12">
+                <div class="item-wrapper">
+                  <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Social Media:</strong></h4>
+                  <p class="mbr-text mbr-fonts-style display-7">Dieses Impressum gilt auch für unsere Social Media
+                    Auftritte bei:<br><strong>Instagram</strong>: <a
+                      href="https://www.instagram.com/feuerwehrreichenbach/"
+                      class="text-secondary">https://www.instagram.com/feuerwehrreichenbach/</a><br><strong>Facebook</strong>:
+                    <a href="https://www.facebook.com/groups/163127135137/"
+                      class="text-secondary">https://www.facebook.com/groups/163127135137/</a><br>
+                  </p>
+                </div>
+              </div>
+              <div class="item features-without-image col-12">
+                <div class="item-wrapper">
+                  <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Haftung für Inhalte</strong>
+                  </h4>
+                  <p class="mbr-text mbr-fonts-style display-7">Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für
+                    eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG
+                    sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
+                    Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
+                    hinweisen.<br><br>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach
+                    den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab
+                    dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
+                    entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br></p>
+                </div>
+              </div>
+              <div class="item features-without-image col-12">
+                <div class="item-wrapper">
+                  <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Urheberrecht</strong></h4>
+                  <p class="mbr-text mbr-fonts-style display-7">Die durch die Seitenbetreiber erstellten Inhalte und
+                    Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung,
+                    Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+                    schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+                    sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.<br></p>
+                </div>
+              </div>
+              <div class="item features-without-image col-12">
+                <div class="item features-without-image col-12">
+                  <div class="item-wrapper">
+                    <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5">
+                      <strong>Datenschutzerklärung:</strong>
+                    </h4>
+                    <p class="mbr-text mbr-fonts-style display-7">Die Datenschutzerklärung ist in vollem Umfang auf
+                      unserer Webseite verfügbar und kann unter folgendem Link eingesehen werden: <a href="/Datenschutz"
+                        class="text-secondary"><?php echo $_SERVER['HTTP_HOST'] ?>/Datenschutz</a>.</p>
+                  </div>
+                </div>
+                <div class="item-wrapper">
+                  <h4 class="mbr-section-subtitle mbr-fonts-style mb-3 display-5"><strong>Online-Streitbeilegung
+                      (OS):</strong></h4>
+                  <p class="mbr-text mbr-fonts-style display-7">Die Europäische Kommission hat eine Plattform zur
+                    Online-Streitbeilegung (OS) eingerichtet, die Sie unter folgendem Link erreichen können: <a
+                      href="https://ec.europa.eu/consumers/odr"
+                      class="text-secondary">https://ec.europa.eu/consumers/odr</a>. Unsere E-Mail-Adresse ist im
+                    Impressum oben aufgeführt.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
-                        <?php include '../assets/includes/socialFooter.php'; ?>
-                      <?php include '../assets/includes/footer.php'; ?>
+  <?php include '../assets/includes/socialFooter.php'; ?>
+  <?php include '../assets/includes/footer.php'; ?>
 
-</body >
+</body>
 
-</html >
+</html>
