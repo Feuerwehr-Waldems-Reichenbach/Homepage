8442b6f3436af5cb13affed6808c1863ca64870d
Tim Bornemann <timbornemann@users.noreply.github.com>
2025-05-28 13:47:49 +0200

    Fix ical creation

--- Public/assets/includes/kalender-download.php
+++ Public/assets/includes/kalender-download.php
@@ -19,22 +19,38 @@ try {
         echo 'Neuigkeit nicht gefunden';
         exit;
     }
-    // Datum verarbeiten - wir nehmen an, dass nur ein Datum in der Datenbank gespeichert ist
-    // Falls Start- und Endzeit benötigt werden, müsste die Datenbankstruktur angepasst werden
+    
+    // Korrekte Verarbeitung des Datums mit Uhrzeit
     $datum = new DateTime($neuigkeit['Datum']);
-    $datumStart = $datum->format('Ymd\THis\Z'); // iCalendar-Format
-    // Enddatum 1 Stunde später (kann nach Bedarf angepasst werden)
-    $datum->modify('+1 hour');
-    $datumEnde = $datum->format('Ymd\THis\Z');
+    $datumStart = $datum->format('Ymd\THis\Z'); // Mit Z für UTC Zeit
+    
+    // Enddatum 4 Stunde später (kann nach Bedarf angepasst werden)
+    $endDatum = clone $datum;
+    $endDatum->modify('+4 hour');
+    $datumEnde = $endDatum->format('Ymd\THis\Z');
+    
     // Zufällige ID für den Kalendereintrag generieren
-    $uid = md5(uniqid(mt_rand(), true)) . '@feuerwehr-website.de';
+    $uid = md5(uniqid(mt_rand(), true)) . '@' . $_SERVER['HTTP_HOST'];
+    
     // Aktuelle Zeit für DTSTAMP
     $jetzt = new DateTime();
     $jetzt = $jetzt->format('Ymd\THis\Z');
+    
+    // Bildanhang vorbereiten, wenn vorhanden
+    $attachment = '';
+    if (!empty($neuigkeit['path_to_image'])) {
+        $imagePath = $_SERVER['DOCUMENT_ROOT'] . '/' . $neuigkeit['path_to_image'];
+        if (file_exists($imagePath)) {
+            // Wir verlinken auf das Bild statt es einzubetten
+            $imageUrl = 'https://' . $_SERVER['HTTP_HOST'] . '/' . ltrim($neuigkeit['path_to_image'], '/');
+            $attachment = "URL:" . $imageUrl . "\r\n";
+        }
+    }
+    
     // iCalendar-Datei erstellen
     $ical = "BEGIN:VCALENDAR\r\n";
     $ical .= "VERSION:2.0\r\n";
-    $ical .= "PRODID:-//Feuerwehr//NONSGML Neuigkeiten//DE\r\n";
+    $ical .= "PRODID:-//Feuerwehr Waldems Reichenbach//Neuigkeiten//DE\r\n";
     $ical .= "METHOD:PUBLISH\r\n";
     $ical .= "BEGIN:VEVENT\r\n";
     $ical .= "UID:" . $uid . "\r\n";
@@ -42,20 +58,48 @@ try {
     $ical .= "DTSTART:" . $datumStart . "\r\n";
     $ical .= "DTEND:" . $datumEnde . "\r\n";
     $ical .= "SUMMARY:" . $neuigkeit['Ueberschrift'] . "\r\n";
+    
     // Beschreibung: Information aus der Datenbank
-    // iCalendar erfordert spezielle Formatierung für mehrzeiligen Text
     $beschreibung = str_replace("\r\n", "\\n", $neuigkeit['Information']);
     $beschreibung = str_replace("\n", "\\n", $beschreibung);
     $ical .= "DESCRIPTION:" . $beschreibung . "\r\n";
+    
     // Ort hinzufügen, falls vorhanden
     if (!empty($neuigkeit['Ort'])) {
         $ical .= "LOCATION:" . $neuigkeit['Ort'] . "\r\n";
     }
+    
+    // Status und Transparenz
+    $ical .= "STATUS:CONFIRMED\r\n";
+    $ical .= "TRANSP:OPAQUE\r\n";
+    $ical .= "SEQUENCE:0\r\n";
+    
+    // Bildanhang hinzufügen, falls vorhanden
+    if (!empty($attachment)) {
+        $ical .= $attachment;
+    }
+    
+    // Benachrichtigung 1 Tag vorher
+    $ical .= "BEGIN:VALARM\r\n";
+    $ical .= "TRIGGER:-P1D\r\n";
+    $ical .= "ACTION:DISPLAY\r\n";
+    $ical .= "DESCRIPTION:Erinnerung: " . $neuigkeit['Ueberschrift'] . "\r\n";
+    $ical .= "END:VALARM\r\n";
+    
+    // Benachrichtigung 1 Stunde vorher
+    $ical .= "BEGIN:VALARM\r\n";
+    $ical .= "TRIGGER:-PT1H\r\n";
+    $ical .= "ACTION:DISPLAY\r\n";
+    $ical .= "DESCRIPTION:Erinnerung: " . $neuigkeit['Ueberschrift'] . "\r\n";
+    $ical .= "END:VALARM\r\n";
+    
     $ical .= "END:VEVENT\r\n";
     $ical .= "END:VCALENDAR\r\n";
+    
     // Header für den Download setzen
     header('Content-Type: text/calendar; charset=utf-8');
     header('Content-Disposition: attachment; filename="Neuigkeit_' . $id . '.ics"');
+    
     // iCalendar-Datei ausgeben
     echo $ical;
 } catch (Exception $e) {
